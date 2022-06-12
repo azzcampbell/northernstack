5c938ad7d79a152e2b05092cad710b5eb42bb865
Gurkage <azz.campbell@gmail.com>
2022-06-12 19:08:49 +0100

    Adding JS and slight CS changes

---
+++ index.js
@@ -0,0 +1,34 @@
+class myHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+        <a href="https://www.northernstack.co.uk">
+        <img class="logo" src="images/logo.svg" alt="logo">
+        </a>
+        <nav>
+            <ul class="nav_links">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </nav>
+        <a class="cta" href="#"><button>Contact</button></a>
+    </header>
+    `
+    }
+}
+
+class myFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+        <div class="footer">
+            <p>Made with <span class="red">❤</span> by Northern Stack 2022&copy</p>
+        </div>
+    </footer>
+    `
+    }
+}
+
+customElements.define('my-header', myHeader)
+customElements.define('my-footer', myFooter)
